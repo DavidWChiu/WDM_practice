977dac7c822bdd7830e1c908a5da4574559930a2
David Chiu <david.chiu@graidtech.com>
2023-02-08 16:30:59 +0800

    Use workitem to create kernel thread

--- MyDriver2019/MyDriver2019/DriverEntry.cpp
+++ MyDriver2019/MyDriver2019/DriverEntry.cpp
@@ -1,36 +1,52 @@
 #include<wdm.h>
 
-PVOID pThreadHandle;
-
-KEVENT EventCloseThread;
-
-void ThreadFunc(PVOID StartContext) 
-{
-    UNREFERENCED_PARAMETER(StartContext);
-    DbgPrint("Hello Kernel Thread!\n");
-    DbgPrint("Kernel Thread starts working!\n");
-    LARGE_INTEGER wait;
-    wait.QuadPart = -10 * 10000000; // 10 seconds
-    while (true) {
-        if (KeWaitForSingleObject(&EventCloseThread, Executive, KernelMode, FALSE, &wait) == STATUS_SUCCESS)
-            break;
-        DbgPrint("Kernel Thread is working!\n");
-    }
-    // DbgBreakPoint();
-    DbgPrint("Bye Kerenl Thread!\n");
-    PsTerminateSystemThread(STATUS_SUCCESS);
-}
-
-EXTERN_C_START
-
-DRIVER_INITIALIZE DriverEntry;
-DRIVER_UNLOAD DriverUnload;
-DRIVER_STARTIO DriverStartIo;
+extern "C" DRIVER_INITIALIZE DriverEntry;
+extern "C" DRIVER_UNLOAD DriverUnload;
+extern "C" DRIVER_STARTIO DriverStartIo;
 IO_WORKITEM_ROUTINE IoWorkitemRoutine;
 
 PDEVICE_OBJECT g_pDeviceObject = NULL;
+PVOID pThreadHandle1;
+PVOID pThreadHandle2;
+KEVENT EventCloseThread;
 
-NTSTATUS
+#define waitTime -10 * 30000000 // 30 seconds
+
+void ThreadFunc1(PVOID StartContext) 
+{
+    UNREFERENCED_PARAMETER(StartContext);
+    DbgPrint("Hello Kernel Thread 1!\n");
+    DbgPrint("Kernel Thread 1 starts working!\n");
+    LARGE_INTEGER wait;
+    wait.QuadPart = waitTime;
+    while (true) {
+        if (KeWaitForSingleObject(&EventCloseThread, Executive, KernelMode, FALSE, &wait) == STATUS_SUCCESS)
+            break;
+        DbgPrint("Kernel Thread 1 is working!\n");
+    }
+    // DbgBreakPoint();
+    DbgPrint("Bye Kerenl Thread 1!\n");
+    PsTerminateSystemThread(STATUS_SUCCESS);
+}
+
+void ThreadFunc2(PVOID StartContext)
+{
+    UNREFERENCED_PARAMETER(StartContext);
+    DbgPrint("Hello Kernel Thread 2!\n");
+    DbgPrint("Kernel Thread 2 starts working!\n");
+    LARGE_INTEGER wait;
+    wait.QuadPart = waitTime;
+    while (true) {
+        if (KeWaitForSingleObject(&EventCloseThread, Executive, KernelMode, FALSE, &wait) == STATUS_SUCCESS)
+            break;
+        DbgPrint("Kernel Thread 2 is working!\n");
+    }
+    // DbgBreakPoint();
+    DbgPrint("Bye Kerenl Thread 2!\n");
+    PsTerminateSystemThread(STATUS_SUCCESS);
+}
+
+extern "C" NTSTATUS
 DriverEntry(
     PDRIVER_OBJECT DriverObject,
     PUNICODE_STRING RegistryPath
@@ -78,16 +94,16 @@ DriverEntry(
     // Practice of creating System Thread.
     KeInitializeEvent(&EventCloseThread, NotificationEvent, FALSE);
     HANDLE hThreadHandle = NULL;
-    status = PsCreateSystemThread(&hThreadHandle, GENERIC_ALL, NULL, NULL, NULL, ThreadFunc, NULL);
+    status = PsCreateSystemThread(&hThreadHandle, GENERIC_ALL, NULL, NULL, NULL, ThreadFunc1, NULL);
     if (status == STATUS_SUCCESS) {
-        ObReferenceObjectByHandle(hThreadHandle, GENERIC_ALL, NULL, KernelMode, &pThreadHandle, NULL);
+        ObReferenceObjectByHandle(hThreadHandle, GENERIC_ALL, NULL, KernelMode, &pThreadHandle1, NULL);
         ZwClose(hThreadHandle);
     }
 
     return status;
 }
 
-void 
+extern "C" void
 DriverUnload(
     PDRIVER_OBJECT DriverObject
 )
@@ -95,10 +111,16 @@ DriverUnload(
     UNREFERENCED_PARAMETER(DriverObject);
     
     KeSetEvent(&EventCloseThread, IO_NO_INCREMENT, FALSE);
-    if (pThreadHandle != NULL) {
-        KeWaitForSingleObject(pThreadHandle, Executive, KernelMode, FALSE, NULL);
-        ObDereferenceObject(pThreadHandle);
-        DbgPrint("Free Thread Handle!\n");
+    if (pThreadHandle1 != NULL) {
+        KeWaitForSingleObject(pThreadHandle1, Executive, KernelMode, FALSE, NULL);
+        ObDereferenceObject(pThreadHandle1);
+        DbgPrint("Free Thread 1 Handle!\n");
+    }
+
+    if (pThreadHandle2 != NULL) {
+        KeWaitForSingleObject(pThreadHandle2, Executive, KernelMode, FALSE, NULL);
+        ObDereferenceObject(pThreadHandle2);
+        DbgPrint("Free Thread 2 Handle!\n");
     }
 
     if (g_pDeviceObject) {
@@ -110,7 +132,8 @@ DriverUnload(
     return;
 }
 
-void DriverStartIo(
+extern "C" void 
+DriverStartIo(
     PDEVICE_OBJECT DeviceObject,
     PIRP Irp
 )
@@ -129,7 +152,15 @@ void IoWorkitemRoutine(
     UNREFERENCED_PARAMETER(DeviceObject);
     UNREFERENCED_PARAMETER(Context);
 
-    DbgPrint("Hello WorkItem!\n");
+    DbgPrint("Hello WorkItem\n");
+
+    HANDLE hThreadHandle = NULL;
+    NTSTATUS status = PsCreateSystemThread(&hThreadHandle, GENERIC_ALL, NULL, NULL, NULL, ThreadFunc2, NULL);
+    if (status == STATUS_SUCCESS) {
+        ObReferenceObjectByHandle(hThreadHandle, GENERIC_ALL, NULL, KernelMode, &pThreadHandle2, NULL);
+        ZwClose(hThreadHandle);
+    }
+
     if (Context != NULL) {
         IoUninitializeWorkItem((PIO_WORKITEM)Context);
         IoFreeWorkItem((PIO_WORKITEM)Context);
@@ -138,5 +169,3 @@ void IoWorkitemRoutine(
     DbgPrint("WorkItem Routine Done\n");
     return;
 }
-
-EXTERN_C_END
